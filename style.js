c758a6163877a5fac22bbc6b38e4e7140f1cc054
Paskulj Boris <117727618+BoriPa@users.noreply.github.com>
2024-02-28 14:14:17 -0300

    Add files via upload

--- style.js
+++ style.js
@@ -4,3 +4,25 @@ const navbarLinks=document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click',()=>{
     navbarLinks.classList.toggle('active')
 })
+
+const goToBtn=document.getElementById("go-top");
+console.log(goToBtn);
+
+window.onscroll= ()=>{
+    scrollFunction();
+}
+function scrollFunction(){
+    if(document.body.scrollTop> 1000 || document.documentElement.scrollTop > 1000){
+        goToBtn.style.display="block"
+    }
+    else{
+        goToBtn.style.display="none"
+    }
+}
+goToBtn.onclick = () => {
+    goToBtn.style.display= "none";
+    window.scroll({
+        top:0,
+        behavior: "smooth"
+    })
+};
